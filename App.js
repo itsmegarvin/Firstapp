33a65d5912cea90c4b2d27ba9eee2140fa416acf
Sagar Pantha <sagresspantha7@gmail.com>
2024-09-18 00:34:52 +0545

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Text,View,} from "react-native";
+
+function App()
+{
+  return(
+    <View>
+        <Text> hello world </Text>
+        <Text> my first one </Text>
+    </View>
+  )
+}
+
+export default App;
+
+
+
+
