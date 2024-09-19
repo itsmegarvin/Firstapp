cff54e1c601d4b62d25099b07556b586fb8b6667
Sagar Pantha <sagresspantha7@gmail.com>
2024-09-19 14:48:10 +0545

    button and onpress events in app.js

--- App.js
+++ App.js
@@ -1,40 +1,29 @@
 import React from "react";
 import { Text, View, Button } from "react-native";
-import VendorDetails from "./components/VendorDetails";
 
-const App = () => {
-  return (
-    <View>
-      <Text style={{ fontSize: 40}}>hello component</Text>
-      <UserData />
-      <CompanyData/>
-      <VendorDetails/>
-    </View>
-  );
-};
+function FirstPress(){
+  console.warn("first button pressed")
+}
 
-//component1
-function UserData() {
-  return (
+const App = () => 
+{
+  function SecondPress(){
+    console.warn("second button pressed")
+  }
+  const ThirdPress= (name)=>{
+    console.warn(name)
+  }
+
+  return(
     <View>
-      <Text style={{fontsize:30}}>Name:garvin poe</Text>
-      <Text>age: 22 </Text>
-      <Text>Email:"garvinpoe@gmail.com"</Text>
+      <Text>Button and onpress event</Text>
+      <Button title="first press"  color={"green"}  onPress={FirstPress} >  </Button>
+      <Text>for second press</Text>
+      <Button title="second press"  color={"blue"}  onPress={SecondPress} />
+      <Text>for third press</Text>
+      <Button title="third press"  color={"skyblue"}  onPress={ ()=>ThirdPress("garvin")} />
     </View>
   );
 }
-
-//component2
-const CompanyData=()=>{
-  return (
-    <View>
-      <Text style={{fontsize:30}}>Name:AutoRobo</Text>
-      <Text>Location:Frankfurt, Germany</Text>
-      <Text>Email:Atro@pvt.ltd.com</Text>
-    </View>
-  );
-}
-
-//for 100's of components it will be tedious so we will make separate folder as component 
 
 export default App;
