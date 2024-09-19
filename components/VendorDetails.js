d5121c60de27c9fa438843dccfb5917267d20e4a
Sagar Pantha <sagresspantha7@gmail.com>
2024-09-19 13:31:50 +0545

    separate vendordetails component made

---
+++ components/VendorDetails.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {Text, View} from "react-native"
+
+const VendorDetails = () =>
+{
+    return (
+      <View>
+        <Text style={{fontsize:30}}>Name: FEDEX electronics supplier</Text>
+        <Text>Location:Berlin , Germany</Text>
+        <Text>Email:fedex@pvt.ltd.com</Text>
+      </View>
+    );
+}
+
+export default VendorDetails;
