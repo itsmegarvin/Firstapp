f3efd1128884b28b78e85c0aee260f90d02bcaf5
Sagar Pantha <sagresspantha7@gmail.com>
2024-09-19 12:31:28 +0545

    jsx concept inside app.js

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import {Text,View,Button} from "react-native";
 
-function App()
+
+let name="garvin"
+function fruit(){
+  return mango;
+}
+
+const App = ()=>
 {
+  const age = 22
   return(
-    <View>  //why View? see takeawaypoint 4
-        <Text style={{fontSize:40}}> first text</Text>
-        <Button title='press me first '></Button>
-        <Text style={{fontSize:40}}> second text</Text> 
-        <Button title='press me second'></Button>
+    <View>  
+        <Text style={{fontSize:40}}> {fruit()} </Text>
+        <Text style={{fontSize:40}}> {name} </Text>
+        <Text style={{fontSize:40}}> {10*20}</Text>
+        <Text style={{fontSize:40}}> {age}</Text>
+        <Text style={{fontSize:40}}> {age>=15 ? "person is adult" : "person is not adult"}</Text>
+        
+
+
+        
         
     </View>
   )
