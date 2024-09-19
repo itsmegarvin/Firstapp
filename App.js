79c4ec5e3cf0426c88b84c67d9d0c6d1e3d42b7a
Sagar Pantha <sagresspantha7@gmail.com>
2024-09-19 13:31:11 +0545

    points added

--- App.js
+++ App.js
@@ -1,33 +1,40 @@
 import React from "react";
-import {Text,View,Button} from "react-native";
+import { Text, View, Button } from "react-native";
+import VendorDetails from "./components/VendorDetails";
 
-
-let name="garvin"
-function fruit(){
-  return mango;
-}
-
-const App = ()=>
-{
-  const age = 22
-  return(
-    <View>  
-        <Text style={{fontSize:40}}> {fruit()} </Text>
-        <Text style={{fontSize:40}}> {name} </Text>
-        <Text style={{fontSize:40}}> {10*20}</Text>
-        <Text style={{fontSize:40}}> {age}</Text>
-        <Text style={{fontSize:40}}> {age>=15 ? "person is adult" : "person is not adult"}</Text>
-        
-
-
-        
-        
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 40}}>hello component</Text>
+      <UserData />
+      <CompanyData/>
+      <VendorDetails/>
     </View>
-  )
+  );
+};
+
+//component1
+function UserData() {
+  return (
+    <View>
+      <Text style={{fontsize:30}}>Name:garvin poe</Text>
+      <Text>age: 22 </Text>
+      <Text>Email:"garvinpoe@gmail.com"</Text>
+    </View>
+  );
 }
+
+//component2
+const CompanyData=()=>{
+  return (
+    <View>
+      <Text style={{fontsize:30}}>Name:AutoRobo</Text>
+      <Text>Location:Frankfurt, Germany</Text>
+      <Text>Email:Atro@pvt.ltd.com</Text>
+    </View>
+  );
+}
+
+//for 100's of components it will be tedious so we will make separate folder as component 
 
 export default App;
-
-
-
-
