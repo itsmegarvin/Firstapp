81a46083d6ef3677a4baccc54a64005756ce2095
Sagar Pantha <sagresspantha7@gmail.com>
2024-09-24 12:46:00 +0545

    TextInput concept in App.js

--- App.js
+++ App.js
@@ -1,54 +1,44 @@
-import React from "react"; 
-import { Text, View, StyleSheet} from "react-native";
-import Exstyles from './src/components/externalStyles'
+import React,{useState} from "react"; 
+import { Text, TextInput, View, StyleSheet, Button} from "react-native";
 
 
 const App= () => //component name always capital
 {
+  const [name, setName] = useState("");
    return(
     <View>
-      <Text style={{fontSize:30, color:"red" }}>INLINE STYLE</Text>
+      <Text style={{fontSize:30, color:"blue" }}>HANDLE TEXTINPUT</Text>
+      <Text style={{fontSize:20}}> Your name is :{name} </Text>
+      
+      <TextInput
+        style={styles.textinput}
+        placeholder="Enter your name"
+        onChangeText={(text)=>setName(text)}
+        value={name} //this makes the value of Textinput to null
+      />
 
-      <Text style={styles.textbox}>INTERNAL STYLE</Text> 
-      <Text style={styles.textbox}>INTERNAL STYLE</Text> 
-      <Text style={Exstyles.textbox}>EXTERNAL STYLE</Text> 
-      <Text style={[styles.textbox, Exstyles.textbox, {borderColor:"pink"}, {borderWidth:5}]}>This is hybrid </Text>
+      <Button title="Clear" onPress={()=>setName("")}/>
     </View>
   );
 }
 
-const styles= StyleSheet.create({
-  textbox:{ //here we can give any name instead textbox and styles
-    color:"#fff",
-    fontSize:40,
-    backgroundColor:"orange",
-    marginBottom:10,
-    padding:10,
-    borderRadius:100,
-    height:100,
-    textAlignVertical:"center",
-    textAlign:"center",
-    borderColor:"red",//bordercolor appears along with borderwidth
-    borderWidth:4
+//internal styling
+const styles = StyleSheet.create({
+  textinput:{
+    fontSize:25, 
+    borderColor:'blue', 
+    borderWidth:2, 
+    margin:20//gives margin from all four sides
   }
 })
+
 
 
 export default App;
 
 /*
-> so styles are inline,internal,external and hybrid
->for inline style={{}}
->for internal style={styles.textbox} and separate component for style as 
- const styles = StyleSheet.create({
-  textbox:{
-    color:blue,
-    fontSize:25
-  }
- })
-
- >for external style we make separate file and make same component as in internal but just replace const with export default. 
- >for hybrid just make array of the objects inside styles.
+>Text input needs state compulsorily as that given input is sometimes needed to be erased/changed/updated
+>onChangeText is only for textInput not for text, button and others
 
 
 */ 
