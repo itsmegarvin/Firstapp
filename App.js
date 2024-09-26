72f0426c1c50f5ccaf7d2d7b217699deace9766e
Sagar Pantha <sagresspantha7@gmail.com>
2024-09-26 18:01:13 +0545

    list with map function in app.js

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from "react"; 
-import { Text, View, FlatList,StyleSheet } from "react-native";
+import { Text, View,StyleSheet, ScrollView } from "react-native";
 
 
 
 const App= () => //component name always capital
 {
-  //you can make state also
   const Users= [//this is array
       {//this is object
         id:1,
@@ -22,21 +21,61 @@ const App= () => //component name always capital
       {
         id:10,
         name:"jake"
+      },
+      {
+        id:14,
+        name:"jacob"
+      },
+      {
+        id:19,
+        name:"aher"
+      },
+      {
+        id:29,
+        name:"jacky"
+      },
+      {
+        id:50,
+        name:"jakson"
+      },
+      {
+        id:55,
+        name:"jakennn"
+      },
+      {
+        id:66,
+        name:"ake"
+      },
+      {
+        id:100,
+        name:"jaa"
+      },
+      {
+        id:101,
+        name:"foden"
+      },
+      {
+        id:150,
+        name:"james"
+      },
+      {
+        id:180,
+        name:"jalbi"
       }
   ]
 
   return(
     <View>
-      <Text style={{fontSize:30}}>Flastlist in RN</Text>
-      <FlatList
-        data={Users}
-        renderItem={({item})=>
-          <Text style={styles.textList}>{item.name}</Text>
-        }
-        //here insided renderItem, parameter should always be item
-        keyExtractor={list=>list.id}
-        //here can be any name instead list eg item iteself or any name abc.
-      />
+      <Text style={{fontSize:50, color:"black"}}>list with map function(custom)</Text>
+      
+      <ScrollView>
+      {
+        Users.map((data)=>
+            <Text style={styles.textList}>{data.name}</Text> //data is like arguement any name instead data.
+        )
+      }
+      </ScrollView>
+      
     </View>
   );
 }
@@ -46,7 +85,7 @@ const styles= StyleSheet.create({
     textList:{
       fontSize:20,
       color:"#fff",
-      margin:10,
+      margin:15,
       backgroundColor:"skyblue",
       borderRadius:20,
       borderColor:"Black",
@@ -60,15 +99,12 @@ export default App;
 
 /*
 >there are different kinds of lists.(flatlist, selection list)
->we can also insert datas dynamically in the list it means the data may not be serial.
-> flatlist may include state with list, or simply variables with list .
-> const list= [
-            {},{},{}
-        ]
-> also list can be like
-   const list= ['hello', 'hi', 'goodmorning', 'goodevening']
-
->so like we make different states,variables for different textinput component similarly, it is good practice to make different flatlist component for different list.
+>But if you want to make list without using properties of RN like Flatlist or selection list then we use map function(custom)
+>basically map is property of javascript, that loops the functions and returns for every loop. that is why it used rather than loops like for, while in javascript.
+>flatlist also uses map function internally.
+>but why flatlist is preferred?
+  - because flatlist has its own property to scroll the view but we need to write code for map. also if there are thousands of item in a list then it lags for map but flatlist easily scrolls as it removes the item that is not showing in the screen.
+>we can encounter various problems while using map function.
 
 */
 
