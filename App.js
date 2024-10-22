2c583be04924a0f64703f09d6c16af38e8e9b658
Sagar Pantha <sagresspantha7@gmail.com>
2024-10-22 18:56:36 +0545

    useEffect for props

--- App.js
+++ App.js
@@ -7,25 +7,34 @@ function App() {
   const [count, setCount] = useState(0);
   const [data, setData] = useState(100);
 
-  useEffect(() => {
-    console.warn(count);
-  },[count]); //we can use as [count, data]
-
-
-  //we can make separate useEffect for different state
-  
-
 
   return (
     <View>
       <Text style={{ fontSize: 30, color: "green" }}>
-              {data} useEffect as componentDidUpdate {count}
+              {data} useEffect for props {count}
       </Text>
       <Button title="increase count" onPress={() => setCount(count + 1)} />
       <Button title="increase data" onPress={() => setData(data + 1)} />
+      <User info={{data,count}}/>
     </View>
   );
 }
+
+const User = (props) => {
+
+  useEffect(()=>{
+    console.warn("this is for count");
+  },[props.info.count])
+
+  return (
+    <View>
+      <Text>data:{props.info.data}  count:{props.info.count}</Text>
+      
+    </View>
+  );
+
+}
+
 
 export default App;
 
