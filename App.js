3850297a09782720121a159d1cb2581ed4630394
Sagar Pantha <sagresspantha7@gmail.com>
2024-10-24 18:47:38 +0545

    useEffect while unmounting

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 // concept of toggle, show and hide
-import React, { useState} from "react";
+import React, { useState, useEffect} from "react";
 import { Text, View, Button } from "react-native";
 
 function App() {
@@ -8,21 +8,23 @@ function App() {
 
   return(
     <View>
-    <Text style={{fontSize:30, color:'orange'}}>Show hide and toggle</Text>
-    {/* <Button  title= "hide component" color={"blue"} onPress={()=>setShow(false)}/>
-    <Button  title= "show component" color={"green"} onPress={()=>setShow(true)}/> */} 
-    <Button  title= "toggle component" color={"green"} onPress={()=>setShow(!show)}/> 
+    <Text style={{fontSize:30, color:'orange'}}>useEffect while unmounting</Text>
+    <Button  title= "toggle " color={"green"} onPress={()=>setShow(!show)}/> 
   
       {
         show ? <User/> : null
       }
   </View>
   );
-  
-
 }
 
+
 const User = () => {
+
+  useEffect(()=>{
+    return ()=>{console.warn("useEffect called")}    
+})
+
   return(
     <View>
       <Text style={{fontSize:20, color:'orange'}}> user component </Text>
@@ -36,5 +38,11 @@ export default App;
 /*
 
 >so this simple concept of toggle, hide and show is how we unmount the UI.
-> this concept is alsoo used in lifecycle methods.
+> so for  LCM (lifecycle methods) basically while unmounting(hiding component), useEffect wont work. But works for showing.
+> IMP Qn
+> why is there return in useEffect hook?
+> we have to make small change in the useEffect adding return function.
+> As a result useEffect is called for unmounting and not for mounting, just opposite than before. 
+> so then what is the use of this unmounting even the UI is hidden from the application?????
+> perfect example is to remove the timers.
 */
