ff41b899ace26f819a8ea79ca0652f8c82cd8e08
Sagar Pantha <sagresspantha7@gmail.com>
2024-11-14 22:23:19 +0545

    activity indicator(loader)

--- App.js
+++ App.js
@@ -1,60 +1,50 @@
-/* Styling button in RN using touchableHighlight and touchableOpacity */
-import React from "react";
-import {Text, View, StyleSheet, TouchableHighlight } from "react-native";
+/* This is all about Activity indicator (loader)*/
+import React,{useState} from "react";
+import { Text, View, Button, StyleSheet, ActivityIndicator } from "react-native";
 
 function App() {
+  const [show,setShow] = useState(false)
+  const displayLoader=()=>{
+    setShow(true)
 
-  return(
+    setTimeout(()=>{
+       setShow(false)
+      },4000);
+  }
+
+  return (
     <View style={styles.main}>
-          <TouchableHighlight>
-            <Text style={styles.button}>Login</Text>
-          </TouchableHighlight>
-          <TouchableHighlight>
-            <Text style={[styles.button, styles.signup]}>Sign up</Text>
-          </TouchableHighlight>
-          <TouchableHighlight>
-            <Text style={[styles.button, styles.warning]}>warning</Text>
-          </TouchableHighlight>
-          <TouchableHighlight>
-            <Text style={[styles.button, styles.error]}>error</Text>
-          </TouchableHighlight>
-        
+      <Text style={{fontSize:30, color:'green'}}>Concept of Activity indicator</Text>
+
+      {/* loader 1 */}
+      <ActivityIndicator size="large" color="skyblue" animating={show}/>
+      <Button title='loader' onPress={displayLoader}/>
+
+      {/* loader 2 */}
+
+      {
+        show ? <ActivityIndicator size="large" color="skyblue"/>  : null 
+      }
+ 
 
     </View>
-    
   );
 }
 
-const styles= StyleSheet.create({
-    main:{
-      flex:1,  // this considers the whole screen.
-      // flexDirection:"row"
-      backgroundColor:"cadetblue",
-      margin:15
-    },
-    button:{
-      fontSize:20,
-      backgroundColor:"midnightblue",
-      color:'#fff',
-      textAlign:"center",
-      margin:10,
-      padding:5,
-      borderRadius:10,
-      shadowColor:"red",
-      elevation:5,
-      shadowOpacity:1
-    },
-    signup:{backgroundColor:"skyblue"},
-    warning:{backgroundColor:"cornflowerblue"},
-    error:{backgroundColor:"midnightblue"}
-
-})
-
-
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+});
 
 export default App;
 
 /*
-> types of blue color (blue,lightblue,darkblue,darkslateblue,skyblue,lightskyblue,lightsteelblue, mediumblue, mediumslateblue, midnightblue, navy, royalblue,slateblue,steelblue,cornflowerblue,cadetblue,dodgerblue)
-> As we know that buttons cannot be styled like Text. But also with the use of touchableHighlight and touchableOpacity we can style the button.
+>so basically the concept of activity indicator(loader) is easy , tyo hamlai screen ma chayeko bela ma ghumeko dekhauxa ni ho tei nai activity indicator ho.
+> Activity indicator is useful when api fetches data/information from the server.
+> We can add more than one activity indicator.
+> In ios there is only three size of the activity loader therfore large, medium and small.
+> In android size of the activity loader therfore can be found as large, medium , small and any number can be given.
 */
