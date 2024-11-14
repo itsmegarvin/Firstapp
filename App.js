90f66562541d00eace37d7a7440e24fd952ca2e2
Sagar Pantha <sagresspantha7@gmail.com>
2024-11-14 23:27:34 +0545

    modal (dialogue box)

--- App.js
+++ App.js
@@ -1,50 +1,73 @@
-/* This is all about Activity indicator (loader)*/
+/* This is all about modal (dialog box)*/
 import React,{useState} from "react";
-import { Text, View, Button, StyleSheet, ActivityIndicator } from "react-native";
+import { Text, View, Button, StyleSheet, Modal } from "react-native";
 
 function App() {
-  const [show,setShow] = useState(false)
-  const displayLoader=()=>{
-    setShow(true)
 
-    setTimeout(()=>{
-       setShow(false)
-      },4000);
-  }
+  const [showModal,setShowModal]= useState(false)
 
-  return (
+  return(
     <View style={styles.main}>
-      <Text style={{fontSize:30, color:'green'}}>Concept of Activity indicator</Text>
-
-      {/* loader 1 */}
-      <ActivityIndicator size="large" color="skyblue" animating={show}/>
-      <Button title='loader' onPress={displayLoader}/>
-
-      {/* loader 2 */}
-
-      {
-        show ? <ActivityIndicator size="large" color="skyblue"/>  : null 
-      }
- 
-
+      <Modal transparent={true} visible={showModal}>
+        <View style= {styles.centerView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Modal message is here for you</Text>
+            <Button title= "close modal" onPress={()=>setShowModal(false)}/>
+          </View>
+        </View>
+      </Modal>
+      <View style={styles.buttonView}>
+        <Button title="open modal" onPress={()=>setShowModal(true)} />
+      </View>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   main: {
-    flex: 1,
-    alignItems:"center",
-    justifyContent:"center"
+    flex: 1
   },
+  buttonView: {
+    flex:1,
+    justifyContent:"flex-end"
+  },
+  centerView: {
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    backgroundColor: 'white',
+    padding: 30,
+    borderRadius: 10,
+    shadowColor: "black",
+    elevation: 10,
+    
+  },
+  modalText: {
+    fontSize:20,
+    padding: 10
+  }
 });
+
+
 
 export default App;
 
+
 /*
->so basically the concept of activity indicator(loader) is easy , tyo hamlai screen ma chayeko bela ma ghumeko dekhauxa ni ho tei nai activity indicator ho.
-> Activity indicator is useful when api fetches data/information from the server.
-> We can add more than one activity indicator.
-> In ios there is only three size of the activity loader therfore large, medium and small.
-> In android size of the activity loader therfore can be found as large, medium , small and any number can be given.
+
+A modal/dialogue box in React Native is a temporary, overlaying component used to display important information or prompt user interaction without navigating away from the main screen.
+
+modal takes the full screen by overwriting the context, so we should make use of transparency.
+
+also use two views component at the starting of the modal.
+
+Focused Content: Overlays the main screen to capture attention.
+Prompt User Decisions: Used for confirmations, alerts, or important actions.
+Show Extra Info: Reveals more details without changing screens.
+Quick Input: Ideal for short forms or data entry.
+Customizable Layout: Allows flexible sizing and positioning for varied content.
+
 */
