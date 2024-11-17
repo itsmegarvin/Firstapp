4fe240c2ba17eacf642755d8bbd67168a263f570
Sagar Pantha <sagresspantha7@gmail.com>
2024-11-17 22:30:47 +0545

    webview npm package

--- App.js
+++ App.js
@@ -1,37 +1,21 @@
-/* Status bar in React-Native */
+/* install npm package (eg webview package) in React-Native */
 import React from "react";
-import { View, Text, StyleSheet, Platform} from "react-native";
+import {WebView } from "react-native-webview";
 
 const App = () => {
   return (
-    <View >
-      <Text style={{fontSize:25, color:"green"}}>platform: {Platform.OS}</Text>
-      {/* for div box view is used*/}
-
-      {/* if os is ios then box is red else orange. */}
-
-      {
-        Platform.OS=='ios'
-        ?
-        <View style={{backgroundColor:'red', height:100, width:100}}></View>
-        :
-        <View style={{backgroundColor:'orange', height:100, width:100}}></View>
-      }
-
-      {/* color of the text for the platform */}
-    <Text style={styles.platformText}>Hello platform</Text>
-
-    </View>
+    <WebView source={{uri:"https://reactnative.dev/"}} />
   );
 };
 
-const styles = StyleSheet.create({
-  platformText: {
-    flex: 1,
-    justifyContent: "center",
-    // color of text for the platforms
-    color:Platform.OS=="android" ? "orange" : "red"
-  },
-});
+
 
 export default App;
+
+
+/*
+first goto terminal and install package then use that package 
+here  we have installed webview package with the Command
+     npm install react-native-webview
+
+*/
