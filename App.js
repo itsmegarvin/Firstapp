8bb2c0dcf21e8c6777f54c32d3ffee75b8b38ee1
Sagar Pantha <sagresspantha7@gmail.com>
2024-11-19 20:50:05 +0545

    custom modal

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-/* install npm package (eg webview package) in React-Native */
-import React from "react";
-import {WebView } from "react-native-webview";
+/* (eg webview package) in React-Native */
+import React, { useState } from "react";
+import { View, Button, Text, StyleSheet } from "react-native";
 
 const App = () => {
+
+  const[show, setShow]= useState(false)
+
   return (
-    <WebView source={{uri:"https://reactnative.dev/"}} />
+    <View style={styles.main}>
+      {
+        show
+        ?
+        <View style={styles.modal}>
+        <View style={styles.body}>
+          <Text>our custom modal</Text>
+          <Button title="close" color="cornflowerblue" onPress={()=>setShow(false)}/>
+        </View>
+        </View>
+        :
+        null
+      }
+      <Button title="open custom modal" color="cornflowerblue" onPress={()=>{setShow(true)}}/>
+    </View>
   );
 };
 
-
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: "rgba(50,50,50,.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  body: {
+    backgroundColor: "#fff",
+    height: 200,
+    width: 300,
+    justifyContent: "flex-end",
+    padding: 20,
+    borderRadius: 10,
+  },
+});
 
 export default App;
 
-
 /*
-first goto terminal and install package then use that package 
-here  we have installed webview package with the Command
-     npm install react-native-webview
+
 
 */
