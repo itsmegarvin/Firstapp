792de08dfd133342c1b078d32153849c87323733
Sagar Pantha <sagresspantha7@gmail.com>
2024-11-21 10:50:34 +0545

    style in stacknavigator

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-/* (eg webview package) in React-Native */
+
 import React from "react";
 import { View, Button, Text, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
@@ -10,9 +10,29 @@ const App = () => {
   return (
     <View style={styles.main}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={Home} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle:{
+              backgroundColor:"skyblue"
+            },
+            headerTitleStyle:{
+              fontSize:20
+            },
+            headerTintColor:"#fff"
+          }}>
+          <Stack.Screen name="Login" component={Login} 
+            />
+          <Stack.Screen name="Home" component={Home} 
+            options={{
+              title:"Home page",
+              headerStyle:{
+                backgroundColor:"skyblue"
+              },
+              headerTitleStyle:{
+                fontSize:20
+              },
+              headerTintColor:"blue"
+            }}/>
         </Stack.Navigator>
       </NavigationContainer>
     </View>
@@ -39,14 +59,18 @@ const Login = (props) => {
 const styles = StyleSheet.create({
   main: {
     flex: 1,
+   
   },
   homepg: {
     flex: 1,
-    alignItems: "center",
+     alignItems:"center",
+    justifyContent:"center"
   },
   loginpg: {
     flex: 1,
-    alignItems: "center",
+    alignItems:"center",
+    justifyContent:"center"
+   
   },
 });
 
@@ -54,5 +78,7 @@ export default App;
 
 /*
 
+>navigation screeen cannot be styled  like other styling properties, we rather use options inside the stackscreen component or navigationcontainer.
+>  we can  do styling  inside navigationcontainer and stack.navigator as well.
 
 */
