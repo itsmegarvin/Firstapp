3245cf01badb300bd4687e29cf2cde0316aaa138
garvin poe <sagresspantha7@gmail.com>
2025-06-01 08:51:18 +0545

    post api with input fields and form data

--- App.js
+++ App.js
@@ -1,44 +1,61 @@
-//simple post api to json server
+// post api from the textinput and simple form.
 
-import React,{useEffect, useState} from "react"
-import {Button,View, Text, StyleSheet} from "react-native"
+import React, { useEffect, useState } from "react";
+import { Button, View, Text, TextInput, StyleSheet, Alert } from "react-native";
 
-function App () 
-{ 
-  const data={
-      "name":"sam",
-      "email":"sam@test",
-      "id":5
-    }
-  
+function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [email, setEmail] = useState("");
 
-  const postApi= async ()=>{
-    const url="http://192.168.101.8:3000/users";
-    const result=  await fetch(url,{
-      method:"POST",
-      headers:{"content-type":"application/json"},
-      body:JSON.stringify(data)
-    })
-    result= await result.json();
+  const saveData = async () => {
+    const url = "http://192.168.101.8:3000/users";
+    const result = await fetch(url, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body :JSON.stringify({name, age, email})
+      });
+    result = await result.json();
   };
 
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>
+        post api with input fields and form data
+      </Text>
+      <TextInput
+        style={styles.inputStyle}
+        value={name}
+        onChangeText={(text) => setName(text)}
+        placeholder="enter name"
+      />
+      <TextInput
+        style={styles.inputStyle}
+        value={age}
+        onChangeText={(text) => setAge(text)}
+        placeholder="enter age"
+      />
+      <TextInput
+        style={styles.inputStyle}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        placeholder="enter email"
+      />
 
-  useEffect(()=>{
-    postApi();
-  },[])
-  return(
-    <View style={styles.main}>
-      <Text style={{fontSize:40}}>simple post api in json server</Text>
-      <Button title="post" onPress={()=>(postApi)}></Button>
+      <Button title="submit" onPress={saveData} />
     </View>
-  )
-};
+  );
+}
 
-const styles= StyleSheet.create({
-    main:{
-      flex:1
-    }
-})
+const styles = StyleSheet.create({
+  inputStyle: {
+    fontSize: 20,
+    borderWidth: 3,
+    borderColor: "skyblue",
+    margin: 10,
+    padding: 10,
+  },
+});
 
 export default App;
 /*
