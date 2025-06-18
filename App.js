217a05935b9e305cf047817b12e135ec7013aed4
garvin poe <sagresspantha7@gmail.com>
2025-06-18 09:53:53 +0545

    ref in react native

--- App.js
+++ App.js
@@ -1,43 +1,47 @@
-//search with api
+//ref in react native
 
-import React, { useEffect, useState } from "react";
-import { View, Text, TextInput, StyleSheet, Alert } from "react-native";
+import React, { useRef  } from "react";
+import { View, Text, TextInput, StyleSheet,Button,Alert } from "react-native";
 
 const App = ()=>{
 
-  const[data,setData]=useState([])
-
-  const SearchUser = async (text)=>{
-    const url=`http://192.168.101.5:3000/users?q=${text}`
-    let result= await fetch(url)
-    result= await result.json()
-    if(result){
-        setData(result)
-    }
+  const input= useRef();
+  
+  const Updateinput=()=>{
+    input.current.focus();
+    input.current.setNativeProps({
+      fontSize:20,
+      color:"green"
+    })
   }
-
   return(
-    <View style={{view:1}}>
-      <TextInput
-        style={{fontSize:20, borderColor:"skyblue", borderWidth:1, margin:15}}
-        placeholder="search"
-        placeholderTextColor={"skyblue"}
-        onChangeText={(text)=>SearchUser(text)}
-      />
-      {
-        data.length
-        ?
-        data.map((item)=>
-          <View >
-            <Text>{item.name}</Text>
-          </View>
-        )
-        :
-        null
-      }
-
+    <View style={styles.main}>
+      <TextInput ref={input} placeholder="Enter name" style={styles.input}/>
+      <TextInput placeholder="Enter Password" style={styles.input}/>
+      <Button title="update input" onPress={Updateinput}/>
     </View>
   )
 }
 
+
+const styles= StyleSheet.create({
+  main:{
+    flex:1,
+    padding:16
+  },
+  input:{
+    fontSize:15,
+    borderColor:"skyblue",
+    borderWidth:3,
+    margin:10
+  }
+})
+
 export default App;
+
+/*
+  >ref is the property of the react js. since RN uses reactjs internally we can use this.
+  >also ref manipulates dom 
+  >meaning it can take the references of the component, make changes on it and yes styling as well.
+  >but always you should use states,variables firstly, ref should be the last option if the changes cannot me made from the states and variables. 
+*/
