e46f94a8159db4ab1130f9b124ae04df037c290e
garvin poe <sagresspantha7@gmail.com>
2025-06-21 17:18:43 +0545

    action on redux using product.js constant.js and action.js

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,26 @@
 import React, { use, useState } from "react";
-import { View, Text, Button, StyleSheet,Image } from "react-native";
+import { View, Text, Button, StyleSheet, Image, Alert } from "react-native";
+import { ADD_TO_CART } from "./redux/action";
 
 const App = (props) => {
-    const item= props.item
+  const item = props.item;
+  const handleAddToCart = (item) => {
+    alert("called")
+  };
   return (
-    <View style={{alignItems:"center",borderBottomColor:"skyblue",borderBottomWidth:5}}>
+    <View
+      style={{
+        alignItems: "center",
+        borderBottomColor: "skyblue",
+        borderBottomWidth: 5,
+      }}
+    >
       <Text style={{ fontSize: 25 }}>{item.name}</Text>
       <Text style={{ fontSize: 25 }}>{item.price}</Text>
       <Text style={{ fontSize: 25 }}>{item.color}</Text>
       <Image style={{ width: 200, height: 200 }} source={{ uri: item.image }} />
       <View style={{ margin: 10 }}>
-        <Button title="add to cart" />
+        <Button title="add to cart" onPress={() => handleAddToCart(item)} />
       </View>
     </View>
   );
